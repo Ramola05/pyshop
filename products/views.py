e44cecfde5be37dc1a4317f59c097b6abd5ccd95
ramola05 <mahajanramola@gmail.com>
2020-05-10 00:24:27 +0530

    edited views file added more functionality

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Product
 
-
+# like when you edit some files
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html',
@@ -10,7 +10,11 @@ def index(request):
 
 
 def new(request):
-    return HttpResponse('New Products')
+    return HttpResponse('New  products will display here')
 
+
+# use vs code instead of pycharmß
+
+#after editing a file or mutiples just save them and do what im gonna do next
 
 
